1cbf3f2ccd4fe4da5a1c3776baffcea3fadbc25a
sweety singh <sweetysingh.1412@gmail.com>
2024-12-08 14:51:44 +0530

    added new file

--- src/components/TranslatorApp.jsx
+++ src/components/TranslatorApp.jsx
@@ -6,22 +6,38 @@ const TranslatorApp = ({ onClose }) => {
   const [showLanguages, setShowLanguages] = useState(false);
   const [currentLanguageSelection, setCurrentLanguageSelection] =
     useState(null);
+  const [inputText, setInputText] = useState("");
+  const [translatedText, setTranslatedText] = useState("");
   const handleLanguageClick = (type) => {
     setCurrentLanguageSelection(type);
     setShowLanguages(true);
   };
-  const handleLanguagesSelect =(languageCode) =>{
-    if (currentLanguageSelection ==='from'){
-      setSelectedLanguageFrom(languageCode)
-    } else{
-      setSelectedLanguageTo(languageCode)
+  const handleLanguagesSelect = (languageCode) => {
+    if (currentLanguageSelection === "from") {
+      setSelectedLanguageFrom(languageCode);
+    } else {
+      setSelectedLanguageTo(languageCode);
     }
-    setShowLanguages(false)
-  }
+    setShowLanguages(false);
+  };
   const handleSwapLanguages = () => {
-    setSelectedLanguageFrom(selectedLanguageTo)
-    setSelectedLanguageTo(selectedLanguageFrom)
+    setSelectedLanguageFrom(selectedLanguageTo);
+    setSelectedLanguageTo(selectedLanguageFrom);
+  };
+  const handleInputChange = (e) =>{
+    const value =e.target.value
   }
+  const handleTranslate = async () => {
+    if (!inputText.trim()) {
+      setTranslatedText("");
+      return;
+    }
+    const response = await fetch(
+      'https://api.mymemory.translated.net/get?q=${encodeURIComponent(inputText,)}&langpair=${selectedLanguageFrom}|{selectedLanguageTo},'
+    );
+    const data = await response.json()
+    setTranslatedText(data.responseData.translatedText)
+  };
   return (
     <div className="w-full flex flex-col gap-y-4 justify-center items-center px-8 pt-12 pb-6 relative">
       <button className="absolute top-4 right-4 ">
@@ -34,9 +50,12 @@ const TranslatorApp = ({ onClose }) => {
         <div className="language" onClick={() => handleLanguageClick("from")}>
           {languages[selectedLanguageFrom] || "English"}
         </div>
-        <i className="fa-solid fa-arrows-rotate tetx-2xl mx-8 cursor-pointer " onClick={handleSwapLanguages}></i>
+        <i
+          className="fa-solid fa-arrows-rotate tetx-2xl mx-8 cursor-pointer "
+          onClick={handleSwapLanguages}
+        ></i>
         <div className="language" onClick={() => handleLanguageClick("to")}>
-        {languages[selectedLanguageTo] || "English"}
+          {languages[selectedLanguageTo] || "English"}
         </div>
       </div>
       {showLanguages && (
@@ -46,7 +65,7 @@ const TranslatorApp = ({ onClose }) => {
               <li
                 className="cursor-pointer hover:bg[#10646b] transition duration-200 p-2 rounded"
                 key={code}
-                onClick={() =>handleLanguagesSelect (code)}
+                onClick={() => handleLanguagesSelect(code)}
               >
                 {name}
               </li>
@@ -56,10 +75,10 @@ const TranslatorApp = ({ onClose }) => {
         </div>
       )}
       <div className="w-full relative">
-        <textarea className="textarea"></textarea>
+        <textarea className="textarea text-gray-200"></textarea>
         <div className="absolute bottom-2 right-4 text-gray-400">0/200</div>
       </div>
-      <button className="w-12 h-12 bg-gradient-to-r from-[#b6f492] to-[#338b93] rounded-full text-2l text-gray-600 flex justify-center items-center active:translate-y-[1px]">
+      <button className="w-12 h-12 bg-gradient-to-r from-[#b6f492] to-[#338b93] rounded-full text-2l text-gray-600 flex justify-center items-center active:translate-y-[1px]" onClick={handleTranslate}>
         <i className="fa-solid fa-chevron-down"></i>
       </button>
       <div className="w-full">
